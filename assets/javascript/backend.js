2289b4f3529f9be0d0794d6887d21a1d773bb599
achrysler <achrysler36@gmail.com>
2018-10-04 13:53:13 -0500

    Added citys object

--- assets/javascript/backend.js
+++ assets/javascript/backend.js
@@ -1,3 +1,132 @@
+//City object
+var locations = [
+  {
+      cityName: "Las Vegas",
+      geoid: "5506956",
+      img: "",
+      namecode: "las-vegas"
+  },
+  {
+      cityName:"Barcelona",
+      geoid: "3128760",
+      img: "",
+      namecode: "barcelona"
+  },
+  {
+      cityName: "Rio",
+      geoid: "3451190",
+      img: "",
+      namecode: "rio-de-janeiro"
+  },
+  {
+      cityName: "Glasgow",
+      geoid: "2648579",
+      img: "",
+      namecode: "glasgow"
+  },
+  {
+      cityName: "Punta Cana",
+      geoid: "3494242",
+      img: "",
+  }, 
+  {
+      cityName: "Shanghai",
+      geoid: "1796236",
+      img: "",
+      namecode: "shanghai"
+  },
+  { 
+      cityName: "Cape Town",
+      geoid: "3369157",
+      img: "",
+      namecode: "cape-town"
+  },
+  { 
+      cityName: "New York",
+      geoid: "5128581",
+      img: "",
+      namecode: "new-york"
+  },
+  {
+      cityName: "Hong Kong",
+      geoid: "1819729",
+      img: "",
+      namecode: "hong-kong"
+  },
+  {
+      cityName: "Phuket",
+      geoid: "1151254",
+      img: "",
+      namecode: "phuket"
+  },
+  { 
+      cityName: "Bangkok",
+      geoid: "1609350",
+      img: "",
+      namecode: "bangkok"
+  }, 
+  {
+      cityName: "Minneapolis",
+      geoid: "5037649",
+      img: "", 
+      namecode: "minneapolis"
+  },
+  {
+      cityName: "Moscow",
+      geoid: "524901",
+      img: "",
+      namecode: "moscow"
+  },
+  {
+      cityName: "Berlin",
+      geoid: "2950159",
+      img: "",
+      namecode: "berlin"
+  },
+  {
+      cityName: "London",
+      geoid: "2643743",
+      img: "",
+      namecode: "london"
+  },
+  {
+      cityName: "Mexico City",
+      geoid: "3530597",
+      img: "",
+      namecode: "mexico-city"
+  },
+  {
+      cityName: "Thebes",
+      geoid: "252910",
+      img: "",
+      namecode: "thebes"
+  },
+  {
+      cityName: "Rome",
+      geoid: "3169070",
+      img: "",
+      namecode: "rome"
+  },
+  {
+      cityName: "Tokyo",
+      geoid: "1850147",
+      img: "",
+      namecode: "tokyo"
+  },
+  {
+      cityName: "Paris",
+      geoid: "2988507",
+      img: "",
+      namecode: "paris"
+  }
+  ];
+
+
+
+
+
+
+
 
 // Set up Firebase
 var config = {
@@ -28,7 +157,7 @@ $.ajax({
 
 //Teleport API (stats)-------------------------------------------------------------------------------------
 
-var cityName = "new-york"
+var cityName = locations[1].cityName
 
 var queryURL = "https://api.teleport.org/api/urban_areas/slug:" + cityName + "/scores";
 
@@ -51,7 +180,7 @@ $.ajax({
 
 //Teleport API (city info)----------------------------------------------\
 
-var geoId = "5391959"
+var geoId = locations[0].geoid
 
 var  cityInfoUrl = "https://api.teleport.org/api/cities/geonameid:" + geoId + "/"
 
