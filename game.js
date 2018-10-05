53d6f06b405692e0443d6f1985fd7ea16040a319
Anthony <tomas.anthony.toledo@gmail.com>
2018-10-05 11:58:51 -0500

    Stuff

--- game.js
+++ game.js
@@ -1,3 +1,9 @@
+// function gameStarted(){
+//     Player1turn=false;
+// }
+
+// gameStarted();
+
 
 //defense is determined by cloud cover, 0-no bonus, 1-3, a 5% bonus,
 //4-6, a 10% bonus, 7-10, a 15% bonus,
@@ -36,65 +42,79 @@ hatk=atk-7.5,hp=hp-5;
 
 
 
-var Player1=Player1
-var Player2=Player1
-var Game= Game;{
-    Player1;{
-        var cloudCover=8 //The cloudCover from the api
-        var atk = -10
+var Game= {
+    
+    
+    
+};
+
+var Player1={
+        cloudCover:8, //The cloudCover from the api
+        p1atk: 10,
+        p1hp:100
+
+        // dfs:0,
+        // if (cloudCover >= 7) {
+        //     dfs = dfs + 15;
+        // } else if (cloudCover >= 4) {
+        //     dfs = dfs + 10;
+        // } else if (cloudCover >= 1)
+        // {
+        //     dfs = dfs + 5;
+        // }
+        // if( temp < 30) {
+        //     catk=atk-5;
+        // } else if (temp > 75)
+        // hatk=atk-7.5,hp=hp-5;
         
-        var dfs = 0
-        if (cloudCover >= 7) {
-            dfs = dfs + 15;
-        } else if (cloudCover >= 4) {
-            dfs = dfs + 10;
-        } else if (cloudCover >= 1)
-        {
-            dfs = dfs + 5;
+        // var win=(p2hp=0)
+        // var p1hp = dfs+100
+        // console.log ("player1 Health", + p1hp)
+    }
+var Player2={
+        p2cloudCover:4, //The cloudCover from the api
+        p2atk: 10,
+        p2hp:100
+        // var dfs = 0
+        // if (p2cloudCover >= 7) {
+        //     dfs = dfs + 15;
+        // } else if (p2cloudCover >= 4) {
+        //     dfs = dfs + 10;
+        // } else if (p2cloudCover >= 1)
+        // {
+        //     dfs = dfs + 5;
+        // }
+        // if( temp < 30) {
+        //     catk=atk-5;
+        // } else if (temp > 75)
+        // hatk=atk-7.5,hp=hp-5;
+        // var p2hp = dfs+100
+        
+        // console.log ("player2 Health", + p2hp);
+
+    }
+    var Player1turn = true
+    var Player2turn = false
+    var Player1frozen = false
+    var Player2frozec = false
+    
+    function attack() {
+        if (Player1turn){
+            Player2.p2hp-=Player1.p1atk;
+            console.log("Player1's Turn");
+            console.log ("player2 Health", + Player2.p2hp);
+            Player1turn=false;
+            Player2turn=true
+        } else if (Player2turn){
+            Player1.p1hp-=Player2.p2atk;
+            console.log ("Player2's Turn");
+            console.log ("player1 Health", + Player1.p1hp);
+            Player2turn=false;
+            Player1turn=true;
         }
-        if( temp < 30) {
-            catk=atk-5;
-        } else if (temp > 75)
-        hatk=atk-7.5,hp=hp-5;
         
-        var win=(p2hp=0)
-        var p1hp = dfs+100
-        console.log ("player1 Health", + p1hp)
-    }
-    Player2;{
-        var p2cloudCover=4 //The cloudCover from the api
-        var atk = -10
-        
-        var dfs = 0
-        if (p2cloudCover >= 7) {
-            dfs = dfs + 15;
-        } else if (p2cloudCover >= 4) {
-            dfs = dfs + 10;
-        } else if (p2cloudCover >= 1)
-        {
-            dfs = dfs + 5;
-        }
-        if( temp < 30) {
-            catk=atk-5;
-        } else if (temp > 75)
-        hatk=atk-7.5,hp=hp-5;
-        var p2hp = dfs+100
-        
-        console.log ("player2 Health", + p2hp);
+    }     
 
-    }
-    var Player1turn=true
-    var Player2turn=false
-    var Player1frozen=false
-    var Player2frozec=false
-    function attack(){
-    if (Player1turn=true){
-        
-    }
-   
-        
-    }
-
-
-
-
+$(document).on("click","#button", function(){
+    attack();
+})
